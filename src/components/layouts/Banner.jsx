3c0059777559e78f9536ad5222153fb5e397fbfd
niazulalam <niazulalam097@gmail.com>
2025-07-20 18:48:34 +0600

    Footer part added

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Container from "../Container";
 import { GrFacebookOption } from "react-icons/gr";
 import { FaTwitter, FaInstagram, FaPinterest } from "react-icons/fa";
@@ -10,8 +10,15 @@ import slideOne from "/src/assets/slideshow-character1.png";
 import slideTwo from "/src/assets/slideshow-character2.png";
 import Slider from "react-slick";
 
+
 const Banner = () => {
   const [activeSlide, setActiveSlide] = useState(0);
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    // Trigger animation on initial render
+    setAnimate(true);
+  }, []);
 
   const settings = {
     dots: true,
@@ -20,9 +27,18 @@ const Banner = () => {
     infinite: true,
     slidesToScroll: 1,
     arrows: false,
-    speed: 1000,
-    autoplaySpeed: 3000,
-    beforeChange: (oldIndex, newIndex) => setActiveSlide(newIndex),
+    pauseOnHover: false,
+    fade: true,
+    // waitForAnimate: false,
+    speed: 2000,
+    autoplaySpeed: 5000,
+    beforeChange: (oldIndex, newIndex) => {
+      setAnimate(false); // Reset animation before changing slide
+      setActiveSlide(newIndex);
+    },
+    afterChange: () => {
+      setAnimate(true); // Trigger animation after slide is shown
+    },
     customPaging: (i) => (
       <div className="w-[35px] h-[35px] flex items-center justify-center relative"></div>
     ),
@@ -35,14 +51,18 @@ const Banner = () => {
 
   return (
     <div className="w-fit border-2 border-bgColor p-2 m-auto relative">
-      <div
-        className={
-          "w-[93vw] bg-[url('/src/assets/bg.png')] bg-no-repeat bg-center bg-cover "
-        }>
+      <div className="w-[93vw] bg-[url('/src/assets/bg.png')] bg-no-repeat bg-center bg-cover">
         <Container>
           <Slider {...settings}>
+            {/* Slide 1 */}
             <div className="pt-[273px] pb-[283px] relative">
-              <div className="">
+              <div
+                className={`transition-all duration-700 ease-in-out transform ${
+                  activeSlide === 0 && animate
+                    ? "translate-y-0 opacity-100"
+                    : "translate-y-10 opacity-0"
+                }`}
+              >
                 <Flex className={"gap-x-2"}>
                   <div className="w-[30px] h-[2.5px] bg-headingTitle"></div>
                   <Heading
@@ -73,13 +93,17 @@ const Banner = () => {
                 src={slideOne}
                 alt={"slideshow-character1.png"}
                 className={`absolute bottom-0 left-[54%] transition-all duration-700 ease-in-out ${
-                  activeSlide === 0
+                  activeSlide === 0 && animate
                     ? "translate-y-0 opacity-100"
                     : "translate-y-10 opacity-0"
                 }`}
               />
               <div className="relative">
-                <p className="uppercase font-bold text-[120px] text-transparent rotate-90 summer absolute bottom-[0%] left-[72%] w-[550px]">
+                <p className={`uppercase font-bold text-[120px] text-transparent rotate-90 summer absolute bottom-[0%] left-[72%] w-[550px] transition-all duration-700 ease-in-out ${
+                  activeSlide === 0 && animate
+                    ? "translate-y-0 opacity-100"
+                    : "translate-y-10 opacity-0"
+                }`}>
                   {
                     <>
                       <span className="font-jost">summ</span>
@@ -89,8 +113,16 @@ const Banner = () => {
                 </p>
               </div>
             </div>
+
+            {/* Slide 2 */}
             <div className="pt-[273px] pb-[283px] relative">
-              <div className="">
+              <div
+                className={`transition-all duration-700 ease-in-out transform ${
+                  activeSlide === 1 && animate
+                    ? "translate-y-0 opacity-100"
+                    : "translate-y-10 opacity-0"
+                }`}
+              >
                 <Flex className={"gap-x-2"}>
                   <div className="w-[30px] h-[2.5px] bg-headingTitle"></div>
                   <Heading
@@ -125,32 +157,36 @@ const Banner = () => {
                 src={slideTwo}
                 alt={"slideshow-character2.png"}
                 className={`absolute -bottom-8 left-[60%] transition-all duration-700 ease-in-out ${
-                  activeSlide === 1
-                    ? "translate-y-0 opacity-100"
-                    : "translate-y-10 opacity-0"
+                  activeSlide === 1 && animate
+                    ? "translate-x-0 opacity-100"
+                    : "translate-x-10 opacity-0"
                 }`}
               />
             </div>
           </Slider>
         </Container>
       </div>
+
+      {/* Social Icons */}
       <div className="absolute top-1/2 -translate-y-1/2 -left-[2%]">
-        <Link to={"https://facebook.com"}>
+        <Link to={"https://www.facebook.com"}>
           <GrFacebookOption className="text-[#767676] mb-8" />
         </Link>
-        <Link to={"https://twitter.com"}>
+        <Link to={"https://www.twitter.com"}>
           <FaTwitter className="text-[#767676] mb-8" />
         </Link>
-        <Link to={"https://instagram.com"}>
+        <Link to={"https://www.instagram.com"}>
           <FaInstagram className="text-[#767676] mb-8" />
         </Link>
-        <Link to={"https://pinterest.com"}>
+        <Link to={"https://www.pinterest.com"}>
           <FaPinterest className="text-[#767676] mb-24" />
         </Link>
         <p className="font-jost font-medium text-sm text-[#767676] uppercase -rotate-90 origin-left pt-[15px]">
           follow us
         </p>
       </div>
+
+      {/* Scroll Indicator */}
       <div className="absolute bottom-[5%] -right-[4.75%] -rotate-90 cursor-pointer">
         <Flex className={"gap-x-3"}>
           <div className="h-[2.5px] w-[35px] bg-menu"></div>
--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,9 +1,196 @@
-import React from 'react'
+import React from "react";
+import Container from "../Container";
+import Logo from "/src/assets/logo.png";
+import Image from "../Image";
+import { Link } from "react-router-dom";
+import { GrFacebookOption } from "react-icons/gr";
+import { FaTwitter, FaInstagram, FaPinterest, FaYoutube } from "react-icons/fa";
+import Flex from "../Flex";
+import Heading from "../Heading";
+import Logos from "/src/assets/payment_logos.png";
+import { GrUp } from "react-icons/gr";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section className="pt-24 pb-8 bg-footerBg">
+      <Container>
+        <div className="grid grid-cols-[27%_16%_16%_16%_25%]">
+          <div className="">
+            <Image src={Logo} alt={"logo.png"} />
+            <p className="font-jost text-sm leading-6 pt-12 pb-7 w-[86%]">
+              1418 River Drive, Suite 35 Cottonhall, CA 9622 United States
+            </p>
+            <p className="font-jost text-sm font-medium pb-2">sale@uomo.com</p>
+            <p className="font-jost text-sm font-medium">+1 246-345-0695</p>
+            <Flex className={"gap-x-5 mt-10"}>
+              <Link to={"https://www.facebook.com"}>
+                <GrFacebookOption className="text-menu" />
+              </Link>
+              <Link to={"https://www.twitter.com"}>
+                <FaTwitter className="text-menu" />
+              </Link>
+              <Link to={"https://www.instagram.com"}>
+                <FaInstagram className="text-menu" />
+              </Link>
+              <Link to={"https://www.instagram.com"}>
+                <FaYoutube className="text-menu text-[19px]" />
+              </Link>
+              <Link to={"https://www.pinterest.com"}>
+                <FaPinterest className="text-menu" />
+              </Link>
+            </Flex>
+          </div>
+          <div className="">
+            <Heading
+              heading={"COMPANY"}
+              className={"font-jost font-medium text-[18px] text-menu mb-10"}
+              as={"h3"}
+            />
+            <ul>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>About Us</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>Careers</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>Affiliates</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>Blog</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit">
+                <Link to={"/"}>Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <Heading
+              heading={"Shop"}
+              className={
+                "uppercase font-jost font-medium text-[18px] text-menu mb-10"
+              }
+              as={"h3"}
+            />
+            <ul>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>New Arrivals</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>Accessories</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>Men</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>Women</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit">
+                <Link to={"/"}>Shop All</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <Heading
+              heading={"help"}
+              className={
+                "uppercase font-jost font-medium text-[18px] text-menu mb-10"
+              }
+              as={"h3"}
+            />
+            <ul>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>Customer Service</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>My Account</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>Find a Store</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>Legal & Privacy</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit mb-6">
+                <Link to={"/"}>Contact</Link>
+              </li>
+              <li className="font-jost text-sm text-menu relative after:inline-block after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-menu after:content-[''] after:transition-all after:duration-300 hover:after:w-7 w-fit">
+                <Link to={"/"}>Gift Card</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <Heading
+              heading={"subscribe"}
+              className={
+                "uppercase font-jost font-medium text-[18px] text-menu mb-8"
+              }
+              as={"h3"}
+            />
+            <p className="font-jost text-sm text-menu w-fit">
+              Be the first to get the latest news about trends, promotions, and
+              much more
+            </p>
+            <form action="/" className="relative">
+              <input
+                type="email"
+                placeholder="Your email address"
+                className="placeholder:text-footerMenu font-jost text-sm py-[20px] px-[21px] bg-white mt-[19px] w-full focus:outline-0"
+              />
+              <input
+                type="submit"
+                value={"join"}
+                className="absolute top-1/2 right-[21px] font-jost font-medium text-sm text-menu uppercase cursor-pointer"></input>
+            </form>
+            <Heading
+              heading={"Secure payments"}
+              className={
+                "font-jost font-medium text-[15px] text-menu pt-11 pb-4"
+              }
+              as={"h4"}
+            />
+            <Image src={Logos} alt={"payment_logos.png"} />
+          </div>
+        </div>
+        <hr className="text-[#cfcdcd] mt-24 mb-10" />
+        <Flex className={"justify-between"}>
+          <p className="font-jost text-sm text-menu">&copy; 2025 Uomo</p>
+          <Flex>
+            <p className="font-jost text-sm text-footerMenu">Language</p>
+            <div className="relative mr-5">
+              <select
+                name="language"
+                id="language"
+                className="font-jost text-sm text-menu pl-5 pr-8 appearance-none focus:outline-0">
+                <option value="english">United Kingdom | English</option>
+                <option value="english">United States | English</option>
+                <option value="german">German</option>
+                <option value="french">French</option>
+                <option value="swedish">Swedish</option>
+              </select>
+              <div className="absolute top-1.5 right-0 pointer-events-none">
+                <GrUp className="ml-6"/>
+              </div>
+            </div>
+            <p className="font-jost text-sm text-footerMenu">Currency</p>
+            <div className="relative">
+              <select
+                name="currency"
+                id="currency"
+                className="appearance-none font-jost text-sm text-menu pl-5 pr-5 focus:outline-0">
+                <option value="usd">$ USD</option>
+                <option value="gbp">&pound; GBP</option>
+                <option value="euro">&euro; EURO</option>
+              </select>
+              <div className="pointer-events-none absolute top-1 right-0 inset-y-0 flex items-center">
+                <GrUp />
+              </div>
+            </div>
+          </Flex>
+        </Flex>
+      </Container>
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
